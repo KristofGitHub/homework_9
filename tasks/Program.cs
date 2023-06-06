c02932368bb5aa00d271b6c2c9c580a48f1bc1ad
Vasiliev Sergei <sergei.binp@gmail.com>
2023-06-06 16:32:12 +0500

    3rd commit: third task done

--- tasks/Program.cs
+++ tasks/Program.cs
@@ -51,8 +51,8 @@ void PrintDiapasone(int n, int m)
 
 int SumDiapasone(int n, int m, int sum)
 {
-    if (n < m) return sum += SumDiapasone(n + 1, m, sum + 1);
-    else if (n > m) return sum += SumDiapasone(n, m + 1, sum + 1);
+    if (n < m) return sum = n + SumDiapasone(n + 1, m, sum);
+    else if (n > m) return sum = m + SumDiapasone(n, m + 1, sum);
     else return sum += n;
 }
 
@@ -61,3 +61,17 @@ int SumDiapasone(int n, int m, int sum)
 // m = 3, n = 2 -> A(m,n) = 29
 Console.WriteLine();
 Console.WriteLine("T A S K   #    3");
+Console.WriteLine("Введите число M: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число N: ");
+int y = Convert.ToInt32(Console.ReadLine());
+int result = 0;
+result = AccermanFunction(x, y, result);
+Console.WriteLine($"Результат равен: {result}");
+
+int AccermanFunction(int m, int n, int result)
+{
+    if(m == 0) {return n + 1;}
+    else if((m > 0) && (n == 0)) {return AccermanFunction(m - 1, 1, result);}
+    else {return AccermanFunction(m - 1, AccermanFunction(m, n - 1, result), result);}
+}
